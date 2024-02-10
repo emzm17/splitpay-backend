76fb924162b7175d411b0159c0138a8b21753272
emzm17 <dibya172000@yahoo.com>
2024-02-10 14:27:11 +0530

    small fix

--- middleware/auth.js
+++ middleware/auth.js
@@ -6,10 +6,10 @@ const auth=(req,res,next)=>{
 
     try{
 
-        let token=req.headers.authorization;
+        const token=req.headers.authorization;
         if(token){
              token=token.split(" ")[1];
-             let user=jwt.verify(token,process.env.SECRET_KEY);
+             const user=jwt.verify(token,process.env.SECRET_KEY);
              req.user_id=user.id;
 
         }
